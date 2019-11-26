2f88d609b212acdcb3233c0ce7db7c9095042796
Talon-Dechert <tdechert@live.com>
2019-11-25 21:22:58 -0800

    Added selected dish functionality

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -6,8 +6,10 @@ function Menu(props) {
   const renderMenuItem = ({ item, index }) => {
     return (
       <ListItem
+        key={index}
         title={item.name}
         subtitle={item.description}
+        onPress={() => props.onPress(item.id)}
         leftAvatar={{ source: require('./images/uthappizza.png') }}
       />
     );
