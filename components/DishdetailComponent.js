3f6197dd3fb5cfe0cdb19d86cd541daf7d41b108
Talon-Dechert <tdechert@live.com>
2019-12-01 20:23:34 -0800

    Added react-navigation stack navigator to the app

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 import { setRecoveryProps } from 'expo/build/ErrorRecovery/ErrorRecovery';
 
 function RenderDish(props) {
@@ -20,8 +21,25 @@ function RenderDish(props) {
   }
 }
 
-function DishDetail(props) {
-  return <RenderDish dish={props.dish} />;
+class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  static navigationOptions = {
+    title: 'Dish Details'
+  };
+
+  render() {
+    //this dishId is how we get access to the props parameter that we passed in by taking the parameter as an argument along with a fallback option
+    const dishId = this.props.navigation.getParam('dishId', '');
+    //the prop passed into render dish will be the dishId as an integer for the index. The plus sign below is strictly to change the type from string to integer
+    // prettier-ignore
+    return (<RenderDish dish={this.state.dishes[+dishId]} />);
+  }
 }
 
 export default DishDetail;
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
-function Menu(props) {
-  const renderMenuItem = ({ item, index }) => {
-    return (
-      <ListItem
-        key={index}
-        title={item.name}
-        subtitle={item.description}
-        onPress={() => props.onPress(item.id)}
-        leftAvatar={{ source: require('./images/uthappizza.png') }}
-      />
-    );
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  //this sets the title of the page in the status bar for react-navigation
+  static navigationOptions = {
+    title: 'Menu'
   };
 
-  return (
-    <FlatList
-      data={props.dishes}
-      renderItem={renderMenuItem}
-      keyExtractor={item => item.id.toString()}
-    />
-  );
+  render() {
+    const { navigate } = this.props.navigation;
+
+    const renderMenuItem = ({ item, index }) => {
+      return (
+        <ListItem
+          key={index}
+          title={item.name}
+          subtitle={item.description}
+          //the navigate here allows for navigation to a new component, the following object contains the props to be sent into that component
+          onPress={() => navigate('DishDetail', { dishId: item.id })}
+          leftAvatar={{ source: require('./images/uthappizza.png') }}
+        />
+      );
+    };
+
+    return (
+      <FlatList
+        data={this.state.dishes}
+        renderItem={renderMenuItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    );
+  }
 }
 
 export default Menu;
