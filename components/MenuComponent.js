6e409e48649f21be8cecb12f03bdfc1a6a0aad0f
Talon-Dechert <tdechert@live.com>
2019-12-05 21:46:27 -0800

    Activity indicator added for loading and error states

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
-import { ListItem, Tile } from 'react-native-elements';
-import { DISHES } from '../shared/dishes';
+import { Tile } from 'react-native-elements';
+import { Loading } from './LoadingComponent';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -33,13 +33,23 @@ class Menu extends Component {
       );
     };
 
-    return (
-      <FlatList
-        data={this.props.dishes.dishes}
-        renderItem={renderMenuItem}
-        keyExtractor={item => item.id.toString()}
-      />
-    );
+    if (this.props.dishes.isLoading) {
+      return <Loading />;
+    } else if (this.props.dishes.errMess) {
+      return (
+        <View>
+          <Text>{this.props.dishes.errMess}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={this.props.dishes.dishes}
+          renderItem={renderMenuItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      );
+    }
   }
 }
 
