b60eb0a8ed1673d054b2dd2fd827dff9824eef1b
Talon-Dechert <tdechert@live.com>
2019-12-11 17:55:19 -0800

    Added a favorite component to keep track of favorite dishes

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -27,6 +27,32 @@ import {
   fetchLeaders
 } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
+
+const FavoritesNavigator = createStackNavigator(
+  {
+    Favorites: { screen: Favorites }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: '#512DA8'
+      },
+      headerTitleStyle: {
+        color: '#fff'
+      },
+      headerTintColor: '#fff',
+      headerLeft: (
+        <Icon
+          name="menu"
+          size={24}
+          color="white"
+          onPress={() => navigation.toggleDrawer()}
+        />
+      )
+    })
+  }
+);
 
 const ReservationNavigator = createStackNavigator(
   {
@@ -278,6 +304,16 @@ const MainNavigator = createDrawerNavigator(
             size={24}
             color={tintColor}
           />
+        )
+      }
+    },
+    Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+        title: 'My Favorites',
+        drawerLabel: 'My Favorites',
+        drawerIcon: ({ tintColor, focused }) => (
+          <Icon name="heart" type="font-awesome" size={24} color={tintColor} />
         )
       }
     }
